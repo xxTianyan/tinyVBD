b6476d9736de6ed561017b9eff4adbc0587b6e88
Tianyan Xu <xuty1999@outlook.com>
2025-12-26 15:16:58 +0800

    add performance monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <functional>
-#include <memory>
 #include <vector>
 #include <raylib.h>
 #include <rlgl.h>
-
+#include <rlImGui.h>
+#include <imgui.h>
 
 #include "RenderHelper.hpp"
 #include "Mesh.h"
@@ -13,11 +13,15 @@
 
 int main(){
 
-    constexpr int screenWidth = 1920;
-    constexpr int screenHeight = 1080;
+    constexpr int screenWidth = 800;  // 1920
+    constexpr int screenHeight = 600; // 1080
 
     SetConfigFlags(FLAG_MSAA_4X_HINT); // anti alias
     InitWindow(screenWidth, screenHeight, "tinyVBD");
+    // initialize rlImGui
+    rlImGuiSetup(true);
+    // Monitor
+    PerformanceMonitor perfMonitor;
 
     // set sample
     HangingCloth falling_cloth;
@@ -39,12 +43,15 @@ int main(){
     // main loop
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        float dt = GetFrameTime() == 0? 1.0f/60 : GetFrameTime();
-
-        auto [x, y] = GetMouseDelta();
-        const float wheel = GetMouseWheelMove();
-        UpdateOrbitCameraInput(orbitCam, Vector2{static_cast<float>(x), static_cast<float>(y)}, wheel,
-                               IsMouseButtonDown(MOUSE_RIGHT_BUTTON), IsMouseButtonDown(MOUSE_MIDDLE_BUTTON));
+        const float dt = GetFrameTime() == 0? 1.0f/60 : GetFrameTime();
+        perfMonitor.Update(dt);
+        // not move camera when mouse is on imgui window
+        if (!ImGui::GetIO().WantCaptureMouse) {
+            auto [x, y] = GetMouseDelta();
+            const float wheel = GetMouseWheelMove();
+            UpdateOrbitCameraInput(orbitCam, Vector2{(float)x, (float)y}, wheel,
+                                   IsMouseButtonDown(MOUSE_RIGHT_BUTTON), IsMouseButtonDown(MOUSE_MIDDLE_BUTTON));
+        }
 
         if (IsKeyPressed(KEY_Z)) {
             ReframeOrbitToModels(orbitCam, models, 1.2f);
@@ -67,27 +74,29 @@ int main(){
             Color{ 45, 93, 125, 255 },   // 顶部：明显更亮的蓝灰
             Color{ 10, 12, 16, 255 }    // 底部：深色
         );
-        DrawFPS(0, 0);
 
         BeginMode3D(orbitCam.camera);
         // draw floor
         DrawModel(falling_cloth.m_floor, Vector3Zero(), 1.0f, WHITE);
-
         // draw cloth
         rlDisableBackfaceCulling();
         for (const auto& m : models) {
             DrawModel(m, Vector3Zero(), 1.0f, DARKBLUE);
         }
         rlEnableBackfaceCulling();
-
+        // draw coordinates
         DrawAxisGizmo(0.8f);
-
         // DrawGrid(10, 1.0f);
-
         EndMode3D();
+
+        rlImGuiBegin();
+        perfMonitor.Draw();
+        rlImGuiEnd();
+
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
 
     return 0;
